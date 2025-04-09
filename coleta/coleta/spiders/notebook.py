bd060017c0da5f7cf33e7bfbfd968af46f1e0719
Marcelo Ricardo <marcello.8@hotmail.com>
2025-04-09 15:48:56 -0300

    Script

---
+++ coleta/coleta/spiders/notebook.py
@@ -0,0 +1,19 @@
+import scrapy
+
+
+class NotebookSpider(scrapy.Spider):
+    name = "notebook"
+    allowed_domains = ["lista.mercadolivre.com.br"]
+    start_urls = [
+        "https://lista.mercadolivre.com.br/notebook?sb%5C=rb%5C#D\[A:notebook\]"]
+
+    def parse(self, response):
+
+        products = response.css('div.ui-search-result__wrapper')
+
+        for product in products:
+            yield {
+                'brand': product.css('span.poly-component__brand::text').get(),
+                'name': product.css('a.poly-component__title::text').get(),
+                'Loja': product.css('span.poly-component__seller::text').get()
+            }
