0d88bc67c84f2037063aa88bfd34d6299c87fd4d
Marcelo Ricardo <marcello.8@hotmail.com>
2025-04-17 11:16:03 -0300

    ETL FEITO

--- Coleta/Transform/main.py
+++ Coleta/Transform/main.py
@@ -12,11 +12,17 @@ df['source'] = "https://lista.mercadolivre.com.br/notebook#D[A:notebook]"
 # Registrar data da raspagem de dados
 df['Data'] = datetime.now()
 
+# Tratando Nulos
+df['old_money'] = df['old_money'].fillna('0')
+df['new_money'] = df['new_money'].fillna('0')
+df['reviews_rating_number'] = df['reviews_rating_number'].fillna('0')
+df['reviews_amount'] = df['reviews_amount'].fillna('0')
+
 # Limpando Colunas
 df['old_money'] = df['old_money'].astype(str).str.replace('.', '', regex=False)
 df['new_money'] = df['new_money'].astype(str).str.replace('.', '', regex=False)
 df['reviews_amount'] = df['reviews_amount'].astype(
-    str).str.replace('[\(\)]', '', regex=False)
+    str).str.replace(r'[()]', '', regex=True)
 
 # conversão para números
 df['old_money'] = df['old_money'].astype(float)
